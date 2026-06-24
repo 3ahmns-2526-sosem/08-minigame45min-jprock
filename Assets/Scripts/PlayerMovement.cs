5ceeaaa8815fcf0db585ac35624937edba5129ac
jakobprock <jakob.prock09@htl-salzburg.ac.at>
2026-06-24 14:59:35 +0200

    Add player movement

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed = 5f;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        rb.linearVelocity = new Vector2(x, y) * speed;
+    }
+}
